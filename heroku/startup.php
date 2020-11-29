78663f2bba6477f631632b6501a37a05cf80e427
jasonspriggs <jason@jasonspriggs.com>
2020-11-29 02:27:21 +0000

    Try shell_exec

--- heroku/startup.php
+++ heroku/startup.php
@@ -7,25 +7,26 @@
 if (getenv("DB_DATABASE") || getenv("DB_HOST") || getenv("DB_USERNAME")) {
   echo "Database Environment variables are manually set. Ignoring add-ins.";
 } else if (getenv("CLEARDB_DATABASE_URL")) {  // ClearDB
-  echo "Using ClearDB Heroku add-in.";
-  $url = getenv('CLEARDB_DATABASE_URL'); exec('export DB_HOST=' . parse_url($url, PHP_URL_HOST));
-  exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  exec('export DB_DATABASE=' . parse_url($url, PHP_URL_PATH));
+  echo "Using ClearDB Heroku add-in.\n";
+  $url = getenv('CLEARDB_DATABASE_URL'); 
+  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
+  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
+  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
+  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
 } else if (getenv("JAWSDB_MARIA_URL")) {      // JawsDB Maria
-  echo "Using JawsDB Maria Heroku add-in.";
+  echo "Using JawsDB Maria Heroku add-in.\n";
   $url = getenv("JAWSDB_MARIA_URL");
-  exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
-  exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  exec('export DB_DATABASE=' . parse_url($url, PHP_URL_PATH));
+  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
+  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
+  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
+  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
 } else if (getenv("JAWSDB_MYSQL_URL")) {      // JawsDB MySQL
-  echo "Using JawsDB MySQL Heroku add-in.";
+  echo "Using JawsDB MySQL Heroku add-in.\n";
   $url = getenv("JAWSDB_MYSQL_URL");
-  exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
-  exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  exec('export DB_DATABASE=' . parse_url($url, PHP_URL_PATH));
+  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
+  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
+  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
+  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
 }
 
 var_dump($_ENV);
