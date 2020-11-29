8af8c79227a0a03ccf9306aab0f23c766f82e53d
jasonspriggs <jason@jasonspriggs.com>
2020-11-29 02:32:18 +0000

    Try generating .env

--- heroku/startup.php
+++ heroku/startup.php
@@ -9,25 +9,23 @@ if (getenv("DB_DATABASE") || getenv("DB_HOST") || getenv("DB_USERNAME")) {
 } else if (getenv("CLEARDB_DATABASE_URL")) {  // ClearDB
   echo "Using ClearDB Heroku add-in.\n";
   $url = getenv('CLEARDB_DATABASE_URL'); 
-  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
-  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
+  file_put_contents(".env", "DB_HOST="     . parse_url($url, PHP_URL_HOST), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_USERNAME=" . parse_url($url, PHP_URL_USER), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_PASSWORD=" . parse_url($url, PHP_URL_PASS), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_DATABASE=" . ltrim(parse_url($url, PHP_URL_PATH), '/'), FILE_APPEND | LOCK_EX);
 } else if (getenv("JAWSDB_MARIA_URL")) {      // JawsDB Maria
   echo "Using JawsDB Maria Heroku add-in.\n";
   $url = getenv("JAWSDB_MARIA_URL");
-  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
-  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
+  file_put_contents(".env", "DB_HOST="     . parse_url($url, PHP_URL_HOST), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_USERNAME=" . parse_url($url, PHP_URL_USER), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_PASSWORD=" . parse_url($url, PHP_URL_PASS), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_DATABASE=" . ltrim(parse_url($url, PHP_URL_PATH), '/'), FILE_APPEND | LOCK_EX);
 } else if (getenv("JAWSDB_MYSQL_URL")) {      // JawsDB MySQL
   echo "Using JawsDB MySQL Heroku add-in.\n";
   $url = getenv("JAWSDB_MYSQL_URL");
-  shell_exec('export DB_HOST='     . parse_url($url, PHP_URL_HOST));
-  shell_exec('export DB_USERNAME=' . parse_url($url, PHP_URL_USER));
-  shell_exec('export DB_PASSWORD=' . parse_url($url, PHP_URL_PASS));
-  shell_exec('export DB_DATABASE=' . ltrim(parse_url($url, PHP_URL_PATH), '/'));
+  file_put_contents(".env", "DB_HOST="     . parse_url($url, PHP_URL_HOST), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_USERNAME=" . parse_url($url, PHP_URL_USER), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_PASSWORD=" . parse_url($url, PHP_URL_PASS), FILE_APPEND | LOCK_EX);
+  file_put_contents(".env", "DB_DATABASE=" . ltrim(parse_url($url, PHP_URL_PATH), '/'), FILE_APPEND | LOCK_EX);
 }
-
-var_dump($_ENV);
 ?>
