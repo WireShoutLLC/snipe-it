1a2fd463e3dd4568a7dea5f5f4683843a5bd603d
jasonspriggs <jason@jasonspriggs.com>
2020-11-29 02:00:11 +0000

    Back to procfile

--- heroku/startup.php
+++ heroku/startup.php
@@ -9,24 +9,24 @@ if (getenv("DB_DATABASE") || getenv("DB_HOST") || getenv("DB_USERNAME")) {
 } else if (getenv("CLEARDB_DATABASE_URL")) {  // ClearDB
   echo "Using ClearDB Heroku add-in.";
   $url = parse_url("CLEARDB_DATABASE_URL");
-  putenv('DB_HOST='     . parse_url("CLEARDB_DATABASE_URL", PHP_URL_HOST));
-  putenv('DB_USERNAME=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_USER));
-  putenv('DB_PASSWORD=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_PASS));
-  putenv('DB_DATABASE=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_PATH));
+  exec('export DB_HOST='     . parse_url("CLEARDB_DATABASE_URL", PHP_URL_HOST));
+  exec('export DB_USERNAME=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_USER));
+  exec('export DB_PASSWORD=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_PASS));
+  exec('export DB_DATABASE=' . parse_url("CLEARDB_DATABASE_URL", PHP_URL_PATH));
 } else if (getenv("JAWSDB_MARIA_URL")) {      // JawsDB Maria
   echo "Using JawsDB Maria Heroku add-in.";
   $url = parse_url("JAWSDB_MARIA_URL");
-  putenv('DB_HOST='     . parse_url("JAWSDB_MARIA_URL", PHP_URL_HOST));
-  putenv('DB_USERNAME=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_USER));
-  putenv('DB_PASSWORD=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_PASS));
-  putenv('DB_DATABASE=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_PATH));
+  exec('export DB_HOST='     . parse_url("JAWSDB_MARIA_URL", PHP_URL_HOST));
+  exec('export DB_USERNAME=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_USER));
+  exec('export DB_PASSWORD=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_PASS));
+  exec('export DB_DATABASE=' . parse_url("JAWSDB_MARIA_URL", PHP_URL_PATH));
 } else if (getenv("JAWSDB_MYSQL_URL")) {      // JawsDB MySQL
   echo "Using JawsDB MySQL Heroku add-in.";
   $url = parse_url("JAWSDB_MYSQL_URL");
-  putenv('DB_HOST='     . parse_url("JAWSDB_MYSQL_URL", PHP_URL_HOST));
-  putenv('DB_USERNAME=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_USER));
-  putenv('DB_PASSWORD=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_PASS));
-  putenv('DB_DATABASE=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_PATH));
+  exec('export DB_HOST='     . parse_url("JAWSDB_MYSQL_URL", PHP_URL_HOST));
+  exec('export DB_USERNAME=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_USER));
+  exec('export DB_PASSWORD=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_PASS));
+  exec('export DB_DATABASE=' . parse_url("JAWSDB_MYSQL_URL", PHP_URL_PATH));
 }
 
 
